ced2c2d8befe68294a0ef772d8b6efe9fc7afaeb
Saras-wata <greatmesaraswata@gmail.com>
2025-08-26 13:04:29 +0000

    fifth commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // important for Render static hosting
+  base: './',   // important for Render static hosting
   build: {
     outDir: 'dist', // Render expects this
   },
