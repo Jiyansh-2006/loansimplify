d4f2762c3fc02302e21f6fd359a770901f165a21
Jiyansh Kushwah <jiyanshkushwahmain@gmail.com>
2025-08-26 14:57:29 +0000

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/loansimplify/",
+  base: "./",
   esbuild: {
     loader: 'jsx',
     include: [/src\/.*\.[jt]sx?$/], // handles .js, .jsx, .ts, .tsx
