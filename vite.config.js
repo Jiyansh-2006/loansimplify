08192168186d0d1fc00916ee69aa95d42831a167
Saras-wata <greatmesaraswata@gmail.com>
2025-08-26 13:08:50 +0000

    sixth commit

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',   // important for Render static hosting
-  build: {
-    outDir: 'dist', // Render expects this
-  },
-  preview: {
-    allowedHosts: ['*'],
-  },
+  base: process.env.VITE_BASE_PATH || "/Hackathon",
+  esbuild: {
+    loader: 'jsx',
+    include: [/src\/.*\.[jt]sx?$/], // handles .js, .jsx, .ts, .tsx
+  }
 })
